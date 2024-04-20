8f705254ba18302eb6ee78d722c6bbe7934c0c55
Karimah Nichols <ms.karimah.nichols@gmail.com>
2024-04-20 08:54:02 -0400

    create constructor for user service repository injection. add promise to controller and service.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/createUser.dto";
+import { UserEntity } from "./user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UserService {
-    async createUser(createDto: CreateUserDto){
-        return createDto;
+    constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    ){}
+
+    async createUser(createUserDto: CreateUserDto):Promise<UserEntity>{
+        const newUser = new UserEntity()
+        Object.assign(newUser, createUserDto);
+        console.log(newUser)
+        return await this.userRepository.save(newUser) //saves entity to the dB
     }
 }
 
+// Object.assign(newUser, createUserDto) takes in a newUser and overwrites it with a data transfer object that we've named createUserDto and assigned a property of CreateUserDto
