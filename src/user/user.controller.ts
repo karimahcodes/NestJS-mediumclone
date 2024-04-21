9ee093c8be235b6910e3fc2878cc65370d117782
Karimah Nichols <ms.karimah.nichols@gmail.com>
2024-04-21 07:21:33 -0400

    test response to post request for new user data transfer object with generic token 'foo'. build user response and create generateJwt function with placeholder token being returned.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,8 +10,11 @@ export class UserController{
     constructor( private readonly userService: UserService){}
 
     @Post('users')       
-    async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserEntity> {
-        return this.userService.createUser(createUserDto);
+    async createUser(
+        @Body('user') createUserDto: CreateUserDto
+    ): Promise<UserEntity> {
+        const user = await this.userService.createUser(createUserDto);
+        return this.userService.buildUserResponse(user)
     }
 }  
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,6 +17,22 @@ export class UserService {
         console.log(newUser)
         return await this.userRepository.save(newUser) //saves entity to the dB
     }
+
+    generateJwt(user: UserEntity): string {
+        return 'foo';
+    }
+//JWT is a string so we want to return a type string
+
+    //build response for your front end
+    buildUserResponse(user: UserEntity): any{
+        return{
+            user: {
+                ...user,
+                token: this.generateJwt(user)
+                //sends back a user object with all of the properties of the original user object and adds a jwt token to the response body
+            }
+        }
+    }
 }
 
 // Object.assign(newUser, createUserDto) takes in a newUser and overwrites it with a data transfer object that we've named createUserDto and assigned a property of CreateUserDto
